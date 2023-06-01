8e075b9d73f58124d741dd92de3ee100b20f445b
Zobeir Hamid <zobeirhamid@berkeley.edu>
2023-06-01 10:22:14 -0700

    chore: showcase configs

--- src/ZeroDevWrapper.tsx
+++ src/ZeroDevWrapper.tsx
@@ -10,11 +10,9 @@ import { connectorsForWallets, RainbowKitProvider, darkTheme } from '@rainbow-me
 import { 
   googleWallet,
   facebookWallet,
-  githubWallet,
-  discordWallet,
-  twitchWallet,
-  twitterWallet,
 } from '@zerodevapp/wagmi/rainbowkit'
+import { KernelAccountAPI } from "@zerodevapp/sdk/dist/src/KernelAccountAPI";
+import { ZeroDevWeb3AuthOptions } from "@zerodevapp/web3auth";
 
 const defaultProjectId = process.env.REACT_APP_ZERODEV_PROJECT_ID || 'b5486fa4-e3d9-450b-8428-646e757c10f6'
 
@@ -23,16 +21,35 @@ const { chains, provider, webSocketProvider } = configureChains(
   [publicProvider()],
 )
 
+const ZERO_DEV_PROJECT_ID = defaultProjectId
+const WEB3AUTH_CLIENT_ID = process.env.REACT_APP_ZEROKIT_WEB3AUTH_CLIENT_ID ?? 'BEjNZMt6TPboj3TfHM06MP8Yxz7cKQX6eK3KZzVhrIMi7jALcZHxJv5o3fDLM7EL4QfPlf2AV_qe155vyR3QxiU'
+const FACTORY_ADDRESS = '0x4E4946298614FC299B50c947289F4aD0572CB9ce'
+
+
+const zeroDevWeb3AuthOptions: ZeroDevWeb3AuthOptions = {
+  adapterSettings: {
+    network: 'mainnet',
+  },
+  web3authOptions: {
+    clientId: WEB3AUTH_CLIENT_ID,
+  },
+};
+
+let zeroDevSocialOptions = {
+  projectId: ZERO_DEV_PROJECT_ID,
+  ...zeroDevWeb3AuthOptions,
+  implementation: {
+    factoryAddress: FACTORY_ADDRESS,
+    accountAPIClass: KernelAccountAPI,
+  },
+};
+
 const connectors = connectorsForWallets([
   {
     groupName: 'Social',
       wallets: [
-        googleWallet({options: { projectId: defaultProjectId}}),
-        facebookWallet({options: { projectId: defaultProjectId}}),
-        githubWallet({options: { projectId: defaultProjectId }}),
-        discordWallet({options: { projectId: defaultProjectId }}),
-        twitchWallet({options: { projectId: defaultProjectId }}),
-        twitterWallet({options: { projectId: defaultProjectId }}),
+        googleWallet({options: zeroDevSocialOptions}),
+        facebookWallet({options: zeroDevSocialOptions}),
     ],
   },
 ]);
