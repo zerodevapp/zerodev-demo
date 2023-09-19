f7d65b58de154f3d6194b29b4a83da1059fc23b4
Zobeir Hamid <zobeirhamid@berkeley.edu>
2023-09-18 18:55:40 -0700

    chore: updates web3auth

--- config-overrides.js
+++ config-overrides.js
@@ -2,15 +2,25 @@ const webpack = require("webpack");
 
 module.exports = function override(config) {
   const fallback = config.resolve.fallback || {};
+  // Object.assign(fallback, {
+  //   crypto: require.resolve("crypto-browserify"),
+  //   stream: require.resolve("stream-browserify"),
+  //   assert: require.resolve("assert/"),
+  //   http: require.resolve("stream-http"),
+  //   https: require.resolve("https-browserify"),
+  //   os: require.resolve("os-browserify"),
+  //   url: require.resolve("url"),
+  //   zlib: require.resolve('browserify-zlib'),
+  // });
   Object.assign(fallback, {
-    crypto: require.resolve("crypto-browserify"),
-    stream: require.resolve("stream-browserify"),
-    assert: require.resolve("assert/"),
-    http: require.resolve("stream-http"),
-    https: require.resolve("https-browserify"),
-    os: require.resolve("os-browserify"),
-    url: require.resolve("url"),
-    zlib: require.resolve('browserify-zlib'),
+    crypto: false,
+    stream: false,
+    assert: false,
+    http: false,
+    https: false,
+    os: false,
+    url: false,
+    zlib: false
   });
   config.resolve.fallback = fallback;
   config.plugins = (config.plugins || []).concat([
