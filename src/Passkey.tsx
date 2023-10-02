899463e8b5356b72bfd1e787ec7bca27c5bfb15e
Zobeir Hamid <zobeirhamid@berkeley.edu>
2023-10-01 18:33:03 -0700

    feat: upgrades passkey

--- src/Passkey.tsx
+++ src/Passkey.tsx
@@ -1,51 +1,81 @@
-import { Button, Flex } from "@mantine/core";
+import { Button, Flex, Input } from "@mantine/core";
 import { chains, projectId } from "./ZeroDevWrapper";
 import { useConnect } from "wagmi";
 import { ZeroDevConnector } from "@zerodev/wagmi";
-import { createPasskeyOwner, getPasskeyOwner, getOrCreatePasskeyOwner } from '@zerodev/sdk/passkey'
-import { useState } from "react";
-
-
+import { createPasskeyOwner, getPasskeyOwner, getAutocompletePasskeyOwner } from '@zerodev/sdk/passkey'
+import { useEffect, useState } from "react";
 
 function Passkey() {
+  const [name, setName] = useState('')
   const [loginLoading, setLoginLoading] = useState(false)
   const [registerLoading, setRegisterLoading] = useState(false)
 
   const { connect } = useConnect()
 
-    const handleEnter = async () => {
-      connect({
-        connector: new ZeroDevConnector({chains, options: {
-          projectId,
-          owner: await getOrCreatePasskeyOwner({name: 'ZeroDev', projectId})
-        }})
-      })
-    }
-
     const handleLogin = async () => {
       setLoginLoading(true)
-      connect({
-        connector: new ZeroDevConnector({chains, options: {
-          projectId,
-          owner: await getPasskeyOwner({projectId})
-        }})
-      })
+      const owner = await getPasskeyOwner({projectId, withCredentials: true, name})
+      if (owner) {
+        connect({
+          connector: new ZeroDevConnector({chains, options: {
+            projectId,
+            owner
+          }})
+        })
+      }
       setTimeout(() => setLoginLoading(false), 5000)
     }
 
     const handleRegister = async () => {
       setRegisterLoading(true)
-      connect({
-        connector: new ZeroDevConnector({chains, options: {
-          projectId,
-          owner: await createPasskeyOwner({name: 'ZeroDev', projectId})
-        }})
-      })
+      const owner = await createPasskeyOwner({name, projectId, withCredentials: true})
+      if (owner) {
+        connect({
+          connector: new ZeroDevConnector({chains, options: {
+            projectId,
+            owner
+          }})
+        })
+      }
       setTimeout(() => setRegisterLoading(false), 5000)
     }
-    
+
+    useEffect(() => {
+      (async () => {
+        const owner = await getAutocompletePasskeyOwner({projectId})
+        if (owner) {
+          connect({
+            connector: new ZeroDevConnector({chains, options: {
+              projectId,
+              owner
+            }})
+          })
+        }
+      })()
+    }, [connect])
 
     return (
+      <>
+        <Input 
+          type="text" 
+          placeholder="Username" 
+          autoComplete="username webauthn" 
+          value={name} 
+          onChange={(e) => setName(e.target.value)} 
+          onFocus={() => {
+            getAutocompletePasskeyOwner({projectId}).then((owner) => {
+              if (owner) {
+                connect({
+                  connector: new ZeroDevConnector({chains, options: {
+                    projectId,
+                    owner
+                  }})
+                })
+              }
+
+            })
+          }}
+        />
         <Flex gap={'lg'}>
           <Button
             loading={loginLoading}
@@ -70,6 +100,7 @@ function Passkey() {
             Enter
           </Button> */}
         </Flex>
+      </>
     )
 
 }
